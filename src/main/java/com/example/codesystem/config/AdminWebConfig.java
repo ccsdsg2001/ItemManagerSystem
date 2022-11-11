0929ca80f309ff1d185f88a58f5a48d65610e1fb
NutF4n <ccsdsg2016@gmail.com>
2022-11-11 22:43:07 +0800

    exception

--- src/main/java/com/example/codesystem/config/AdminWebConfig.java
+++ src/main/java/com/example/codesystem/config/AdminWebConfig.java
@@ -2,6 +2,7 @@ package com.example.codesystem.config;
 
 import com.example.codesystem.Interceptor.loginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author cc
  * @date 2022年11月04日 21:44
  */
-
+@Configuration
 public class AdminWebConfig implements WebMvcConfigurer {
 
     @Override
