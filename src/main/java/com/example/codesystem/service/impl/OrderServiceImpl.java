78453dfa237e74bc2416e7c443df3d96023c14e4
NutF4n <ccsdsg2016@gmail.com>
2022-11-23 23:21:17 +0800

    page access

--- src/main/java/com/example/codesystem/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/codesystem/service/impl/OrderServiceImpl.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -24,6 +25,8 @@ import java.util.List;
 public class OrderServiceImpl implements OrderService {
 
 
+//    @Autowired
+//    @Resource
     @Autowired
     OrderItemMapper orderItemMapper;
 
