7fa75540bae38618aecbe8d6ad60ddca6d77c8d3
NutF4n <ccsdsg2016@gmail.com>
2022-11-25 22:38:41 +0800

    ItemEdit Delete

--- src/main/java/com/example/codesystem/Controller/itemController.java
+++ src/main/java/com/example/codesystem/Controller/itemController.java
@@ -2,15 +2,21 @@ package com.example.codesystem.Controller;
 
 import com.example.codesystem.model.Item;
 import com.example.codesystem.model.ItemCategory;
+import com.example.codesystem.model.ReItem;
+import com.example.codesystem.model.ResObject;
 import com.example.codesystem.service.ItemCategoryService;
 import com.example.codesystem.service.ItemService;
+import com.example.codesystem.service.ReItemService;
+import com.example.codesystem.util.Constant;
 import com.example.codesystem.util.PageUtil;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author cc
@@ -20,6 +26,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class itemController {
     @Autowired
     ItemService itemService;
+    @Autowired
+    ReItemService reItemService;
 
 
     @Autowired
@@ -36,4 +44,49 @@ public class itemController {
 
         return "item/itemManage";
     }
+
+    @ResponseBody
+    @PostMapping("/user/itemEditState")
+    public ResObject<Object> itemEditState(Item item1) {
+        Item item = itemService.findById(item1);
+        ReItem reItem = new ReItem();
+        reItem.setId(item.getId());
+        reItem.setBarcode(item.getBarcode());
+        reItem.setCid(item.getCid());
+        reItem.setImage(item.getImage());
+        reItem.setPrice(item.getPrice());
+        reItem.setNum(item.getNum());
+        reItem.setSellPoint(item.getSellPoint());
+        reItem.setStatus(item.getStatus());
+        reItem.setTitle(item.getTitle());
+        reItem.setRecovered(new Date());
+        reItemService.insert(reItem);
+        itemService.delete(item1);
+        ResObject<Object> object = new ResObject<Object>(Constant.Code01, Constant.Msg01, null, null);
+        return object;
+
+    }
+
+    @GetMapping("/user/itemEdit")
+    public String itemEdit(Model model,Item item){
+        ItemCategory itemCategory = new ItemCategory();
+        itemCategory.setStart(0);
+        itemCategory.setEnd(Integer.MAX_VALUE);
+
+        List<ItemCategory> list = itemCategoryService.list(itemCategory);
+        model.addAttribute("itemCategoryList",list);
+        if(item.getId()!=0){
+            Item item1 = itemService.findById(item);
+            String id =String.valueOf(item.getId());
+
+
+
+
+        }
+
+        return "item/itemEdit";
+    }
+
+
+
 }
